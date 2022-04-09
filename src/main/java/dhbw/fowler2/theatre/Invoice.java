24ce58ba84219ca8d762e80c1fc3e5c5a513d3d0
Basti029 <102850316+Basti029@users.noreply.github.com>
2022-04-09 13:38:27 +0200

    Refactoring #1

--- src/main/java/dhbw/fowler2/theatre/Invoice.java
+++ src/main/java/dhbw/fowler2/theatre/Invoice.java
@@ -11,4 +11,51 @@ public class Invoice {
         this.customer = customer;
         this.performances = performances;
     }
+
+    public double calculation() {
+        double totalamount = 0;
+        for (var perf : invoice.performances) {
+            var play = plays.get(perf.playID);
+            var thisAmount = 0;
+
+            switch (play.type) {
+                case "tragedy":
+                    thisAmount = 40000;
+                    if (perf.audience > 30) {
+                        thisAmount += 1000 * (perf.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if (perf.audience > 20) {
+                        thisAmount += 10000 + 500 * (perf.audience - 20);
+                    }
+                    thisAmount += 300 * perf.audience;
+                    break;
+                default:
+                    throw new Error("unknown type: ${play.type}");
+            }
+            // print line for this order
+            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            totalAmount += thisAmount;
+        }
+        return totalAmount;
+    }
+
+    public double creditCalculation() {
+        double totalamount = 0;
+        int volumeCredits = 0;
+        for (var perf : invoice.performances) {
+            var play = plays.get(perf.playID);
+
+            // add volume credits
+            volumeCredits += Math.max(perf.audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        }
+        return volumeCredits;
+    }
+
+
 }
